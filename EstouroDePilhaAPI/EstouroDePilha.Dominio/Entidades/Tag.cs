9b83f6c2e2c7536c8b4f3a5b0f6e21b4f32e8b87
Leonardo34 <leo_broch@hotmail.com>
2017-07-05 22:31:37 -0300

    feat(WebAPI Dominio): Adicionar entidade Tag

--- EstouroDePilhaAPI/EstouroDePilha.Dominio/Entidades/Tag.cs
+++ EstouroDePilhaAPI/EstouroDePilha.Dominio/Entidades/Tag.cs
@@ -6,16 +6,10 @@ using System.Threading.Tasks;
 
 namespace EstouroDePilha.Dominio.Entidades
 {
-    public class Usuario
+    public class Tag
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Endereco { get; set; }
         public string Descricao { get; set; }
-        public DateTime DataCadastro { get; set; }
-        public string UrlFotoPerfil { get; set; }
-        public string Senha { get; set; }
         public List<Pergunta> Perguntas { get; set; }
-        public List<Resposta> Respostas { get; set; }
     }
 }
