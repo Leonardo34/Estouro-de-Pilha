d4124b568152b41d54b031d4bb2f90ca45af056a
mateusforgiarini <mateus.silva@cwi.com.br>
2017-07-06 10:58:54 -0300

    feat (repositorio) implementar interface RepositorioBase em tags, perguntas e repostas.

--- EstouroDePilhaAPI/EstouroDePilha.Infraestrutura/Repositórios/PerguntaRepositorio.cs
+++ EstouroDePilhaAPI/EstouroDePilha.Infraestrutura/Repositórios/PerguntaRepositorio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EstouroDePilha.Dominio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,41 @@ using System.Threading.Tasks;
 
 namespace EstouroDePilha.Infraestrutura.Repositórios
 {
-    public class PerguntaRepositorio
+    public class PerguntaRepositorio : RepositorioBase<Pergunta>
     {
         private Contexto contexto;
 
         public PerguntaRepositorio(Contexto contexto)
         {
             this.contexto = contexto;
+        }
+
+        public void Alterar(Pergunta pergunta)
+        {
+            contexto.Entry(pergunta).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void Criar(Pergunta pergunta)
+        {
+            contexto.Perguntas.Add(pergunta);
+            contexto.SaveChanges();
+        }
+
+        public void Deletar(Pergunta pergunta)
+        {
+            contexto.Perguntas.Remove(pergunta);
+            contexto.SaveChanges();
+        }
+
+        public List<Pergunta> Listar()
+        {
+            return contexto.Perguntas.ToList();
+        }
+
+        public Pergunta ObterPorId(int id)
+        {
+            return contexto.Perguntas.FirstOrDefault(p => p.Id == id);
         }
     }
 }
