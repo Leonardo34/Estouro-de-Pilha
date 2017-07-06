7d03c90b3c27f9343a4c0a9fd86dc19f969aaa3d
LeonardoBMorais <leonardo.morais@cwi.com.br>
2017-07-05 21:16:30 -0300

    feat(API): Adicionar camada de domínio e modelagem da classe Usuario

---
+++ EstouroDePilhaAPI/EstouroDePilha.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstouroDePilha.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Descricao { get; set; }
+        public string UrlFotoPerfil { get; set; }
+        public string Senha { get; set; }
+    }
+}
