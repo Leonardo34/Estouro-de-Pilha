3f3ebe20948cdd4f94874bef3963dc6add198c35
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-06 17:02:49 -0300

    feat(Login): adicionar enpoint na api.

--- EstouroDePilhaAPI/EstouroDePilhaAPI/Controllers/UsuarioController.cs
+++ EstouroDePilhaAPI/EstouroDePilhaAPI/Controllers/UsuarioController.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 
@@ -64,15 +65,10 @@ namespace EstouroDePilhaAPI.Controllers
         }
 
         [BasicAuthorization]
-        [HttpPost]
-        public HttpResponseMessage Criar(Usuario usuario)
-        {          
-            if (!usuario.IsValid())
-            {
-                throw new Exception();
-            }
-            repositorio.Criar(usuario);
-            return ResponderOK(usuario);
+        [HttpGet, Route("login")]
+        public HttpResponseMessage FazerLogin()
+        {
+            return ResponderOK(repositorio.ObterPorEmail(Thread.CurrentPrincipal.Identity.Name));
         }
 
         [BasicAuthorization]
