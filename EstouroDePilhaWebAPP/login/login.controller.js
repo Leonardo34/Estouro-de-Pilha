a51d3a7505f12522cd6147b842bf1f9fef24f60c
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-07-06 15:36:43 -0300

    feat(login): implementar loginController no frontend e ajustar layout do login e home.

--- EstouroDePilhaWebAPP/login/login.controller.js
+++ EstouroDePilhaWebAPP/login/login.controller.js
@@ -1,3 +1,13 @@
-app.controller('loginController', function($scope, $location, authService) { 
-    
+app.controller('loginController', function($scope, $location, authService) {
+  $scope.url.path = $location.path();
+
+  $scope.login = function() {
+      authService.login($scope.usuario).then(response => {
+          console.log(response);
+          alert('Ta logado Tchê!');
+      }, error => {
+          console.log(error);
+          alert('Desculpa tchê, mas alguma coisa ta errada!');
+      })
+  }
 });
