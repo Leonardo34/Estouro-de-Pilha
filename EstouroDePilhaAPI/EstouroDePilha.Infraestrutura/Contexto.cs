9662e51736484b570bd6842ee85e8c3e6ca09195
mateusforgiarini <mateus.silva@cwi.com.br>
2017-07-05 22:31:26 -0300

    feat (Infraestrutura) adicionar contexto e EntityFramework.

---
+++ EstouroDePilhaAPI/EstouroDePilha.Infraestrutura/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstouroDePilha.Infraestrutura
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("ExemploEFSP")
+        {
+
+        }
+
+    }
+}
