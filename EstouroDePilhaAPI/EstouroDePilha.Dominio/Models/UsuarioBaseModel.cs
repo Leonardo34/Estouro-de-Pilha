2c1a28fc620d8dda14a834bfd41f184dce202a4e
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-06 18:20:20 -0300

    feat(model usuario): adicionar classe model base de usuario.

---
+++ EstouroDePilhaAPI/EstouroDePilha.Dominio/Models/UsuarioBaseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstouroDePilhaAPI.Models
+{
+    public class UsuarioBaseModel
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+
+        public UsuarioBaseModel (int id, string nome, string email)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Email = email;
+        }       
+    }
+}
