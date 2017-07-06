c1b6a6705a71d98cc128b4d0fe2f1ebad0b87515
Leonardo de Morais <leonardo.morais@cwi.com.br>
2017-07-06 09:40:46 -0300

    feat(WebAPI Infraestrutura): Adicionar mapeamento das entidades Tag e Usuario

---
+++ EstouroDePilhaAPI/EstouroDePilha.Infraestrutura/Mapeamento/TagMap.cs
@@ -0,0 +1,27 @@
+﻿using EstouroDePilha.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstouroDePilha.Infraestrutura.Mapeamento
+{
+    public class TagMap : EntityTypeConfiguration<Tag>
+    {
+        public TagMap()
+        {
+            ToTable("Tag");
+
+            HasMany(x => x.Perguntas)
+                .WithMany(x => x.Tags)
+                .Map(x =>
+                {
+                    x.MapLeftKey("IdTag");
+                    x.MapRightKey("IdPergunta");
+                    x.ToTable("TagPergunta");
+                });
+        }
+    }
+}
---
+++ EstouroDePilhaAPI/EstouroDePilha.Infraestrutura/Mapeamento/UsuarioMap.cs
@@ -0,0 +1,26 @@
+﻿using EstouroDePilha.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstouroDePilha.Infraestrutura.Mapeamento
+{
+    public class UsuarioMap : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMap()
+        {
+            ToTable("Usuario");
+
+            HasMany(x => x.Perguntas)
+                .WithRequired(x => x.Usuario)
+                .Map(x => x.MapKey("IdUsuario"));
+
+            HasMany(x => x.Respostas)
+                .WithRequired(x => x.Usuario)
+                .Map(x => x.MapKey("IdUsuario"));
+        }
+    }
+}
