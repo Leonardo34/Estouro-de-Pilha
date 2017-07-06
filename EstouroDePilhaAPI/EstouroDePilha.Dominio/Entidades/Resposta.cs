223f03660280822f31daa1fdbcfdd5b5eb88e25d
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-05 21:45:32 -0300

    feat(API Dominio): Adicionar entidade Resposta

---
+++ EstouroDePilhaAPI/EstouroDePilha.Dominio/Entidades/Resposta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstouroDePilha.Dominio.Entidades
+{
+    public class Resposta
+    {
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public String Descricao { get; set; }
+    }
+}
