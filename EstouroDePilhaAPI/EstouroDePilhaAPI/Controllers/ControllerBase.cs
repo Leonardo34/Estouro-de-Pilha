c3cafa8f466c54eaf1eaaee181bd7b7114fc5918
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-06 00:30:16 -0300

    feat(API Controller): Adicionar controller abstrata

---
+++ EstouroDePilhaAPI/EstouroDePilhaAPI/Controllers/ControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using EstouroDePilha.Infraestrutura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EstouroDePilhaAPI.Controllers
+{
+    public abstract class ControllerBase : ApiController
+    {
+        readonly Contexto contexto;
+        public ControllerBase(Contexto contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        public void SalvarAlteracoes()
+        {
+            this.contexto.SaveChanges();
+        }
+
+    }
+}
