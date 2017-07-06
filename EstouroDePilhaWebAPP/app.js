a2801e7f6f75ff8cb4fec13d6259b8725811ffad
Leonardo34 <leo_broch@hotmail.com>
2017-07-05 23:28:51 -0300

    feat(WebApp): Adicionar estrutura inicial da aplicação Web

---
+++ EstouroDePilhaWebAPP/app.js
@@ -0,0 +1,18 @@
+var app = angular.module("EstouroPilhaApp", ['ngRoute', 'auth']);
+
+
+
+angular.module('EstouroPilhaApp').constant('authConfig', {
+
+    // Obrigatória - URL da API que retorna o usuário
+    urlUsuario: '...',
+
+    // Obrigatória - URL da aplicação que possui o formulário de login
+    urlLogin: '/login',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGIN com sucesso
+    urlPrivado: '/home',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGOUT
+    urlLogout: '/login'
+}); 
