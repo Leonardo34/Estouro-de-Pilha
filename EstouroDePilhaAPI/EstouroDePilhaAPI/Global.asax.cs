067de234e77cb0adb40a83b627c0f2de72934055
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-06 22:17:27 -0300

    feat(injecao de dependencia): adicionar container e registros em app start.

--- EstouroDePilhaAPI/EstouroDePilhaAPI/Global.asax.cs
+++ EstouroDePilhaAPI/EstouroDePilhaAPI/Global.asax.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using SimpleInjector.Integration.WebApi;
+using EstouroDePilha.Dominio.Repositórios;
+using EstouroDePilha.Infraestrutura.Repositórios;
 
 namespace EstouroDePilhaAPI
 {
@@ -12,6 +17,21 @@ namespace EstouroDePilhaAPI
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            container.Register<IPerguntaRepositorio, PerguntaRepositorio>(Lifestyle.Scoped);
+            container.Register<IRespostaRepositorio, RespostaRepositorio>(Lifestyle.Scoped);
+            container.Register<ITagRepositorio, TagRepositorio>(Lifestyle.Scoped);
+            container.Register<IUsuarioRepositorio, UsuarioRepositorio>(Lifestyle.Scoped);
+
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver =
+                new SimpleInjectorWebApiDependencyResolver(container);
         }
     }
 }
