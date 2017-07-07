14d9f930b1a50c2222182a04da14e8ea8072e29b
Leonardo34 <leo_broch@hotmail.com>
2017-07-07 13:22:54 -0300

    feat(WebAPI): Criar model para pergunta

---
+++ EstouroDePilhaAPI/EstouroDePilha.Dominio/Models/PerguntaModel.cs
@@ -0,0 +1,17 @@
+﻿using EstouroDePilhaAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstouroDePilha.Dominio.Models
+{
+    public class PerguntaModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public UsuarioBaseModel Usuario { get; set; }
+    }
+}
