ecd24378e372828b836d63bccc221b2457eb0663
Leonardo34 <leo_broch@hotmail.com>
2017-07-06 22:57:08 -0300

    feat(WebAPI PerguntaController): Adicionar decoração de autenticação e criar método para obter pergunta através do id e permitir também a criação de perguntas

--- EstouroDePilhaAPI/EstouroDePilhaAPI/Controllers/PerguntaController.cs
+++ EstouroDePilhaAPI/EstouroDePilhaAPI/Controllers/PerguntaController.cs
@@ -1,10 +1,12 @@
 ﻿using EstouroDePilha.Dominio.Entidades;
 using EstouroDePilha.Infraestrutura;
 using EstouroDePilha.Infraestrutura.Repositórios;
+using EstouroDePilhaAPI.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 
@@ -13,41 +15,58 @@ namespace EstouroDePilhaAPI.Controllers
     [RoutePrefix("api/perguntas")]
     public class PerguntaController : ControllerBase
     {
-        private PerguntaRepositorio repositorio = new PerguntaRepositorio(Contexto.contexto);
+        private PerguntaRepositorio perguntasRepositorio = new PerguntaRepositorio(Contexto.contexto);
+        private UsuarioRepositorio usuarioRepositorio = new UsuarioRepositorio(Contexto.contexto);
 
+        [BasicAuthorization]
         [HttpGet]
         public HttpResponseMessage ListarPerguntas()
         {
-            var perguntas = repositorio.Listar();
+            var perguntas = perguntasRepositorio.Listar();
             return ResponderOK(perguntas);
         }
 
+        [BasicAuthorization]
         [HttpDelete]
         public HttpResponseMessage Deletar(Pergunta pergunta)
         {
-            if (repositorio.ObterPorId(pergunta.Id) == null)
+            if (perguntasRepositorio.ObterPorId(pergunta.Id) == null)
             {
                 throw new Exception();
             }
-            repositorio.Deletar(pergunta);
+            perguntasRepositorio.Deletar(pergunta);
             return ResponderOK(pergunta);
         }
 
+        [BasicAuthorization]
         [HttpPost]
+        [Route("nova")]
         public HttpResponseMessage Criar(Pergunta pergunta)
         {
-            repositorio.Criar(pergunta);
+            pergunta.Usuario = 
+                usuarioRepositorio.ObterPorEmail(Thread.CurrentPrincipal.Identity.Name);
+            perguntasRepositorio.Criar(pergunta);
             return ResponderOK(pergunta);
         }
 
+        [BasicAuthorization]
         [HttpPut]
         public HttpResponseMessage Alterar(Pergunta pergunta)
         {
-            if (repositorio.ObterPorId(pergunta.Id) == null)
+            if (perguntasRepositorio.ObterPorId(pergunta.Id) == null)
             {
                 throw new Exception();
             }
             return ResponderOK(pergunta);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        [BasicAuthorization]
+        public HttpResponseMessage ObterPorId(int id)
+        {
+            var pergunta = perguntasRepositorio.ObterPorId(id);
+            return ResponderOK(new { descricao = pergunta.Descricao, idUsuario = pergunta.Usuario.Id });
         }
     }
 }
