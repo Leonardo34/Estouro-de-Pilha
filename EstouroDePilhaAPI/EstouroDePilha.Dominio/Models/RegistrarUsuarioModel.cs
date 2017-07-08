f8655087050ee3392e8b0cc577d09bd30996f45d
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-07 21:30:29 -0300

    feat(listagem usuario): adicionar mais campos na model.

--- EstouroDePilhaAPI/EstouroDePilha.Dominio/Models/RegistrarUsuarioModel.cs
+++ EstouroDePilhaAPI/EstouroDePilha.Dominio/Models/RegistrarUsuarioModel.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace EstouroDePilhaAPI.Models
-{
-    public class RegistrarUsuarioModel : UsuarioBaseModel
-    {       
-        public string Senha { get; set; }
-        public string Descricao { get; set; }
-        public string Endereco { get; set; }
-
-        public RegistrarUsuarioModel(int id, string nome, string email, string senha, string urlImagemPerfil, string endereco, string descricao) : base(id, nome, email, urlImagemPerfil)
-        {
-            this.Senha = senha;
-            this.Endereco = endereco;
-            this.Descricao = descricao;
-        }
-    }
+﻿namespace EstouroDePilhaAPI.Models
+{
+    public class RegistrarUsuarioModel : UsuarioBaseModel
+    {       
+        public string Senha { get; set; }
+        public RegistrarUsuarioModel(int id, string nome, string email, string senha, 
+            string urlImagemPerfil, string endereco, string descricao) 
+            : base(id, nome, email, urlImagemPerfil, endereco,  descricao)
+        {
+            this.Senha = senha;        
+        }
+    }
 }
