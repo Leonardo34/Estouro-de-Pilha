b9ecbbd5559d4657d6281f930418cb48554bda23
mateusforgiarini <mateus.silva@cwi.com.br>
2017-07-08 11:57:06 -0300

    fix (buscarPergunta) corrigir caminho na controller.

--- EstouroDePilhaWebAPP/pesquisarPergunta/pesquisar.pergunta.service.js
+++ EstouroDePilhaWebAPP/pesquisarPergunta/pesquisar.pergunta.service.js
@@ -1,7 +1,7 @@
 angular.module('EstouroPilhaApp').factory('pesquisarPerguntaService', function ($http){
 
   var urlPerguntas = "http://localhost:53986/api/perguntas/paginacaopesquisa/"
-  var urlNumeroderesultados = "http://localhost:53986/api/perguntas/pesquisa/numeroderesultados"
+  var urlNumeroderesultados = "http://localhost:53986/api/perguntas/pesquisa/numeroderesultados/"
 
   function   buscarPerguntaPorTitulo(perguntaPesquisada, paginaAtual){
     return $http.get(urlPerguntas + perguntaPesquisada + "/" + paginaAtual);
