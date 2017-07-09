9ad0deb139d2e7ef400a96d04c43a95fe5aa058a
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-09 20:36:34 -0300

    feat(cadastro de pergunta): adicionar markdown automaticamente clicando em botão.

--- EstouroDePilhaWebAPP/cadastroPergunta/cadastro.pergunta.controller.js
+++ EstouroDePilhaWebAPP/cadastroPergunta/cadastro.pergunta.controller.js
@@ -3,6 +3,7 @@ angular.module('EstouroPilhaApp')
 
 function cadastrarPerguntaController($scope, $routeParams, authService, cadastroPerguntaService) { 
   $scope.cadastrarPergunta = cadastrarPergunta;
+  $scope.adicionarMarkdown = adicionarMarkdown;
   
 
   function cadastrarPergunta(novaPergunta) {
@@ -13,4 +14,37 @@ function cadastrarPerguntaController($scope, $routeParams, authService, cadastro
         alert('Alguma coisa deu errada, tenta de novo, Tchê!');
       })
   } 
+
+  function adicionarMarkdown (tipo) {
+    let texto = angular.copy($scope.novaPergunta.Descricao);
+    let novoTexto = "";
+    let selecao = window.getSelection().toString().trim();
+    let resultado = "";
+    let textarea = angular.element(document.getElementById("descricao"));
+    switch(tipo) {
+      case 'B':
+        resultado = `**${selecao}**`;
+        break;
+      case 'I':
+        resultado = `_${selecao}_`;
+        break;
+      case 'OL':
+        resultado = `\n1. ${selecao}`;
+        break;
+      case 'UL':
+        resultado = `\n* ${selecao}`;
+        break;
+      case 'C':
+        resultado = '``'+ selecao + '``';
+        break;
+      case 'H':       
+        resultado = 
+        selecao.charAt(1) === ' '  || selecao.charAt(1) === '#'  ? 
+              `#${selecao}` : `# ${selecao}`;
+        break;      
+    }
+
+    novoTexto = texto.replace(selecao, resultado);
+    $scope.novaPergunta.Descricao = novoTexto;
+  }
 }
