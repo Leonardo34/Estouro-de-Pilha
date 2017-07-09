463e8b62b315c3f2a3efb9dafab5aefd5469751b
BrunoAguiar <bruno.aguiar@cwi.com.br>
2017-07-09 17:31:00 -0300

    fix(cadastroPergunta): ajustar controller do cadastro de pergunta e fazer pergunta pelo frontend.

--- EstouroDePilhaWebAPP/cadastroPergunta/cadastro.pergunta.controller.js
+++ EstouroDePilhaWebAPP/cadastroPergunta/cadastro.pergunta.controller.js
@@ -1,11 +1,12 @@
-angular.module('EstouroPilhaApp').controller('cadastrarPerguntaController', function ($scope, authService, perguntaService){
+angular.module('EstouroPilhaApp').controller('cadastrarPerguntaController', 
+                      function($scope, $routeParams, authService, cadastroPerguntaService){
 
   $scope.cadastrarPergunta = function(novaPergunta) {
-    cadastroService.cadastrarPergunta(novaPergunta)
-            .then(response => {
-              alert('Pergunta cadastrada com sucesso, Tchê!')
-            }, error => {
-              alert('Alguma coisa deu errada, tenta de novo, Tchê!');
-            })
+    cadastroPerguntaService.cadastrarPergunta(novaPergunta)
+      .then(response => {
+        alert('Pergunta cadastrada com sucesso, Tchê!')
+      }, error => {
+        alert('Alguma coisa deu errada, tenta de novo, Tchê!');
+      })
    }
 });
