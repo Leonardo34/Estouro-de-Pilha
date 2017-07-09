3041472f325b2cbed1133e29ab0043dcad06d70d
Leonardo34 <leo_broch@hotmail.com>
2017-07-08 22:35:34 -0300

    feat(WebAPP HomePage): Adicionar implementação inicial da home-page com nav para páginação, e apresentação das perguntas recentes

--- EstouroDePilhaWebAPP/home/home.controller.js
+++ EstouroDePilhaWebAPP/home/home.controller.js
@@ -1,4 +1,14 @@
-angular.module('EstouroPilhaApp').controller('homeController', function ($scope, authService){
+angular.module('EstouroPilhaApp').controller('homeController', function ($scope, authService, perguntaService){
+  $scope.pagina = 0;
+  $scope.itensPagina = 5;
+  carregarPerguntas();
 
-
+  function carregarPerguntas() {
+    let skip = $scope.pagina * $scope.itensPagina;
+    perguntaService.buscarPerguntasPaginadas(skip, $scope.itensPagina).then(res => {
+      $scope.perguntas = res.data.result;
+      console.log($scope.perguntas);
+      $scope.pagina += 1;
+    })
+  }
 });
