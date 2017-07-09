6e5411e8a101a5d15fb6ebc306ac51a352b3787f
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-09 19:26:40 -0300

    refactor(pergunta controller): indentar codigo.

--- EstouroDePilhaWebAPP/cadastroPergunta/cadastro.pergunta.controller.js
+++ EstouroDePilhaWebAPP/cadastroPergunta/cadastro.pergunta.controller.js
@@ -1,12 +1,16 @@
-angular.module('EstouroPilhaApp').controller('cadastrarPerguntaController', 
-                      function($scope, $routeParams, authService, cadastroPerguntaService){
+angular.module('EstouroPilhaApp')
+  .controller('cadastrarPerguntaController', cadastrarPerguntaController);
 
-  $scope.cadastrarPergunta = function(novaPergunta) {
+function cadastrarPerguntaController($scope, $routeParams, authService, cadastroPerguntaService) { 
+  $scope.cadastrarPergunta = cadastrarPergunta;
+  
+
+  function cadastrarPergunta(novaPergunta) {
     cadastroPerguntaService.cadastrarPergunta(novaPergunta)
       .then(response => {
-        alert('Pergunta cadastrada com sucesso, Tchê!')
+        alert('Pergunta cadastrada com sucesso, Tchê!');
       }, error => {
         alert('Alguma coisa deu errada, tenta de novo, Tchê!');
       })
-   }
-});
+  } 
+}
