118927e2cf375e0b83267e22c9ecce611e4e6e3e
Leonardo34 <leo_broch@hotmail.com>
2017-07-10 17:13:44 -0300

    feat(WebAPP Cadastro pergunta): Adicionar form para adicionar tags a uma pergunta e com auto-complete

--- EstouroDePilhaWebAPP/cadastroPergunta/cadastro.pergunta.controller.js
+++ EstouroDePilhaWebAPP/cadastroPergunta/cadastro.pergunta.controller.js
@@ -1,9 +1,12 @@
 angular.module('EstouroPilhaApp')
   .controller('cadastrarPerguntaController', cadastrarPerguntaController);
 
-function cadastrarPerguntaController($scope, $routeParams, authService, cadastroPerguntaService) {
+function cadastrarPerguntaController($scope, $routeParams, authService, cadastroPerguntaService, tagService) {
   $scope.cadastrarPergunta = cadastrarPergunta;
   $scope.adicionarMarkdown = adicionarMarkdown;
+  $scope.tagsSelecionadas = [];
+
+  buscarTags();
 
   function adicionarMarkdown (tipo){
     $scope.novaPergunta.Descricao = 
@@ -11,11 +14,37 @@ function cadastrarPerguntaController($scope, $routeParams, authService, cadastro
   } 
 
   function cadastrarPergunta(novaPergunta) {
+    novaPergunta.TagsIds = $scope.tagsSelecionadas.map(t => $scope.tags.find(x => x.Descricao == t).Id);
+    console.log(novaPergunta);
     cadastroPerguntaService.cadastrarPergunta(novaPergunta)
       .then(response => {
         alert('Pergunta cadastrada com sucesso, Tchê!');
       }, error => {
         alert('Alguma coisa deu errada, tenta de novo, Tchê!');
       })
-  }  
+  }
+
+  function buscarTags() {
+    tagService.pegarTodasTags().then(res => {
+      $scope.tags = res.data.result;
+      console.log($scope.tags);
+    })
+  }
+
+  $scope.autocompleteTag = function(str, tags) {
+    var matches = [];
+    tags.forEach(tag => {
+      if (tag.Descricao.includes(str)) {
+        matches.push(tag);
+      }
+    })
+    return matches;
+  }
+
+  $scope.adicionarTag = function(nomeTag) {
+    if (!$scope.tagsSelecionadas.some(t => t === nomeTag)) {
+      $scope.tagsSelecionadas.push(nomeTag);
+      console.log($scope.tagsSelecionadas);
+    } 
+  }
 }
