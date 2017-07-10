f3a4394326dc7b575d667e4fadd1ae48b0c566cc
Leonardo34 <leo_broch@hotmail.com>
2017-07-09 21:26:13 -0300

    feat(WebAPP PerguntaController): Adicionar funções ao escopo da controller para dar upvote/downvote em uma resposta

--- EstouroDePilhaWebAPP/pergunta/pergunta.controller.js
+++ EstouroDePilhaWebAPP/pergunta/pergunta.controller.js
@@ -1,19 +1,31 @@
-angular.module('EstouroPilhaApp').controller('perguntaController', function ($scope, $routeParams, authService, perguntaService){
+angular.module('EstouroPilhaApp').controller('perguntaController', function ($scope, $routeParams, authService, perguntaService) {
 
   var idDaPergunta = $routeParams.id;
 
   buscarPerguntaPorId(idDaPergunta);
   buscarRespostaPorIdDaPergunta(idDaPergunta);
+  $scope.upvoteResposta = upvoteResposta;
+  $scope.downvoteResposta = downvoteResposta;
 
   function buscarPerguntaPorId(idDaPergunta){
-    perguntaService.buscarPerguntaPorId(idDaPergunta).then(function (response){
+    perguntaService.buscarPerguntaPorId(idDaPergunta).then(function (response) {
       $scope.pergunta = response.data.result;
     })
   }
 
-  function buscarRespostaPorIdDaPergunta(idDaPergunta){
-    perguntaService.buscarRespostaPorIdDaPergunta(idDaPergunta).then(function (response){
+  function buscarRespostaPorIdDaPergunta(idDaPergunta) {
+    perguntaService.buscarRespostaPorIdDaPergunta(idDaPergunta).then(function (response) {
       $scope.respostas = response.data.result;
     })
+  }
+
+  function upvoteResposta(resposta) {
+    perguntaService.upvoteResposta(resposta.Id);
+    resposta.QuantidadeUpVotes += 1;
+  }
+
+  function downvoteResposta(resposta) {
+    perguntaService.downvoteResposta(resposta.Id);
+    resposta.QuantidadeDownVotes += 1;
   }
 });
