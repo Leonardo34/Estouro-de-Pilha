070f859d491d2acab4318abf6e92312f0638a357
mateusforgiarini <mateus.silva@cwi.com.br>
2017-07-10 18:46:03 -0300

    feat (editar pergunta) implementar backend.

--- EstouroDePilhaAPI/EstouroDePilhaAPI/Controllers/PerguntaController.cs
+++ EstouroDePilhaAPI/EstouroDePilhaAPI/Controllers/PerguntaController.cs
@@ -50,16 +50,12 @@ namespace EstouroDePilhaAPI.Controllers
             return ResponderOK(pergunta);
         }
 
-        [BasicAuthorization]
-        [HttpPost]
-        [Route("nova")]
-        public HttpResponseMessage Criar(PerguntaModel perguntaModel)
+        private Pergunta CriarPergunta(PerguntaModel perguntaModel)
         {
             var pergunta = new Pergunta();
             pergunta.Tags = new List<Tag>();
             pergunta.Usuario =
                 usuarioRepositorio.ObterPorEmail(Thread.CurrentPrincipal.Identity.Name);
-            pergunta.DataPergunta = DateTime.Now;
             pergunta.Titulo = perguntaModel.Titulo;
             pergunta.Descricao = perguntaModel.Descricao;
             if (perguntaModel.TagsIds != null)
@@ -74,18 +70,27 @@ namespace EstouroDePilhaAPI.Controllers
             {
                 throw new Exception();
             }
+            return pergunta;
+        }
+
+        [BasicAuthorization]
+        [HttpPost]
+        [Route("nova")]
+        public HttpResponseMessage Criar(PerguntaModel perguntaModel)
+        {
+            var pergunta =  CriarPergunta(perguntaModel);
+            pergunta.DataPergunta = DateTime.Now;
             perguntasRepositorio.Criar(pergunta);
             return ResponderOK(new { id = pergunta.Id });
         }
 
         [BasicAuthorization]
         [HttpPut]
-        public HttpResponseMessage Alterar(Pergunta pergunta)
+        [Route()]
+        public HttpResponseMessage Alterar([FromBody]PerguntaModel perguntaModel)
         {
-            if (perguntasRepositorio.ObterPorId(pergunta.Id) == null)
-            {
-                throw new Exception();
-            }
+            var pergunta = CriarPergunta(perguntaModel);
+            perguntasRepositorio.Alterar(pergunta);
             return ResponderOK(pergunta);
         }
 
