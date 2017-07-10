8c162d38172524ba1b2fce7dce557bb0e0da58f1
Leonardo34 <leo_broch@hotmail.com>
2017-07-10 09:44:18 -0300

    feat(WebAPP Down/Up Vote): Adicionar funcionalidade para downvote e upvote

--- EstouroDePilhaWebAPP/pergunta/pergunta.controller.js
+++ EstouroDePilhaWebAPP/pergunta/pergunta.controller.js
@@ -6,6 +6,10 @@ angular.module('EstouroPilhaApp').controller('perguntaController', function ($sc
   buscarRespostaPorIdDaPergunta(idDaPergunta);
   $scope.upvoteResposta = upvoteResposta;
   $scope.downvoteResposta = downvoteResposta;
+  $scope.usuarioVotouEmResposta = usuarioVotouEmResposta;
+  $scope.usuarioDeuUpvoteResposta = usuarioDeuUpvoteResposta;
+  $scope.usuarioDeuDownvoteResposta = usuarioDeuDownvoteResposta;
+  $scope.usuario = authService.getUsuario();
 
   function buscarPerguntaPorId(idDaPergunta){
     perguntaService.buscarPerguntaPorId(idDaPergunta).then(function (response) {
@@ -22,10 +26,24 @@ angular.module('EstouroPilhaApp').controller('perguntaController', function ($sc
   function upvoteResposta(resposta) {
     perguntaService.upvoteResposta(resposta.Id);
     resposta.QuantidadeUpVotes += 1;
+    resposta.UpVotes.push($scope.usuario);
   }
 
   function downvoteResposta(resposta) {
     perguntaService.downvoteResposta(resposta.Id);
     resposta.QuantidadeDownVotes += 1;
+    resposta.DownVotes.push($scope.usuario);
+  }
+
+  function usuarioVotouEmResposta(resposta) {
+    return usuarioDeuDownvoteResposta(resposta) || usuarioDeuUpvoteResposta(resposta);
+  }
+
+  function usuarioDeuDownvoteResposta(resposta) {
+    return resposta.DownVotes.some(u => u.Id == authService.getUsuario().Id);
+  }
+
+  function usuarioDeuUpvoteResposta(resposta) {
+    return resposta.UpVotes.some(u => u.Id == authService.getUsuario().Id);
   }
 });
