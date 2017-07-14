bc447d5d3a26bf7733818c553fa92bfbb0b41589
Leonardo Alves <leonardo.alves@cwi.com.br>
2017-07-14 01:04:01 -0300

    feat(home): adicionar models para retornar apenas dados necessarios.

---
+++ EstouroDePilhaAPI/EstouroDePilha.Dominio/Models/PerguntaHomeModel.cs
@@ -0,0 +1,22 @@
+﻿using EstouroDePilha.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstouroDePilha.Dominio.Models
+{
+    public class PerguntaHomeModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public UsuarioPerguntaHomeModel Usuario { get; set; }
+        public PerguntaHomeModel(int id, string titulo, int idUsuario, string url, string nomeUsuario, List<Badge> badges)
+        {
+            Usuario = new UsuarioPerguntaHomeModel(idUsuario, url, nomeUsuario, badges);
+            Id = id;
+            Titulo = titulo;
+        }
+    }
+}
